df47cb28fb457e6d24302c4d55b5d117d7a8e599
Matthew McCormack <mmccormack1182@gmail.com>
2018-09-26 13:32:04 -0400

    Tests Passed

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require 'pry'
 
 def key_for_min_value(name_hash)
-
+  if name_hash.length == 0
+    return nil
+  end
+  lowest_key =
+  lowest_value = 0
+  name_hash.each do |key, value|
+    if lowest_value == 0
+      lowest_value = value
+      lowest_key = key
+    elsif lowest_value > value
+      lowest_key = key
+      lowest_value
+    end
+  end
+  lowest_key
 end
