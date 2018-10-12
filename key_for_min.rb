f1def434ed64a00311ba4404c199daff89583108
Brandon Teich <brandonteich123@gmail.com>
2018-10-12 03:52:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,21 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(hash)
+  if hash == {}
+    nil
+  end
+  temp_key = ""
+  temp_value = ""
+  name_hash.each do |key, value| #blake: 500
+    if (temp_value == "") #if first time we run and temp_value hasnt been assigned, assign it
+      temp_key = key
+      temp_value = value
 
+    elsif (value < temp_value) #if we have a temp_value, compare the current value to it
+      temp_key = key
+      temp_value = value
+    end
+  end
+  temp_key
 end
