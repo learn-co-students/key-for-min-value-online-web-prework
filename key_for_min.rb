b0f91894c0ea4757dcc76cf7de8482b4434ebc96
Andrew Johnson <ajohnson.uva@gmail.com>
2018-10-19 14:01:01 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,21 +4,19 @@
 require 'pry'
 def key_for_min_value(name_hash)
   x = nil
+  y = nil
   name_array = []
-  name_hash.collect do |name, num|
-    if x == nil || num < x
-      x = num
+  name_hash.each do |name, num|
+    if y == nil || num < y
+      y = num
     end
   end
-  name_hash.collect do |name, num|
-    if name_hash[name] == x
-      name_array << name
-      return name
+  name_hash.each do |name, num|
+    if name_hash[name] == y
+      x = name
     end
   end
-  if name_array == []
-    return nil
-  end
+  x
 end
 # if x == nil || num < x
 # x = num
