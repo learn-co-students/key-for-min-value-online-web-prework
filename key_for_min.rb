f2010fc850f7faeca67c607ec73c19b3a1a37bee
Lu Daley <daleyluis@gmail.com>
2019-01-30 18:56:19 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,30 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  index = 0 
+name_hash.collect do |key,value|  
+  if counter < name_hash.length
+    #{key} #{value}
+    index += 1
+    
 
+
+#returns the key of the smallest hash value
 end
+
+=begin
+ does not call the `#keys` method
+  does not call the `#values` method
+  does not call the `#min` method
+  does not call the `#sort` method
+  does not call the `#min_by` method
+
+#if value <
+#value that is smallest/"<"  all other values (compared to what?)
+#return key (only?) of smallest value
+
+
+#if key.first < :suite_a && name.start_with?("A")
+ #   winner = name
+#end
+=end
