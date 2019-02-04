06d07f77dd917127b7cbd8e199201cdca201e209
Kyle Spencer <spencerk2@vcu.edu>
2019-02-04 03:10:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,17 @@
 
 def key_for_min_value(name_hash)
 
+current_value = nil
+  
+  name_hash.each do
+    	|key, value|
+    	
+            if current_value == nil || value < current_value
+            then current_value = value
+            end
+        
+        							end
+         
+    name_hash.key(current_value)
+
 end
