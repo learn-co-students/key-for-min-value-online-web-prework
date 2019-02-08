5fec2b5799581c0ed3b9b9d9f844c22bfd642f00
Allen F Partlow <allenfp@gmail.com>
2019-02-08 02:42:59 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,17 @@
 def key_for_min_value(name_hash)
   hash_num = 0
   lowest_hash_key = ""
+  if name_hash == {}
+    return
+  end
 
   name_hash.collect do |key, value|
     if hash_num == 0
       hash_num = value
       lowest_hash_key = key
-    elsif hash_num < value
+    elsif hash_num > value
       hash_num = value
       lowest_hash_key = key
-
-
 
     end
   end
