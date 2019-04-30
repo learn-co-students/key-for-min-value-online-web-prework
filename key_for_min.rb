5f6c3cc1048447eb41847dfd8bba1fe6fe8366b0
Felipe Bohorquez <fbohorqu@hotmail.com>
2019-04-30 02:41:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+if name_hash.empty?
+ 	puts "Hey! No block was given!"
+
+else
+  hash_min(name_hash)
+end
+end
+
+def hash_min(hash)
+`
+
 
 end
