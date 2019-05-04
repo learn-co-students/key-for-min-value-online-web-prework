67905950143919b08591b3be6b805b1aa763b235
Lindsey Wells <lindsey.wells84@gmail.com>
2019-05-04 20:54:34 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,11 +4,15 @@ sample_hash={:blake => 500, :ashley => 2, :adam => 1}
 
 def key_for_min_value(name_hash)
   lowest_name = nil
-  lowest_value = 1000000
+  lowest_value = nil
 
   name_hash.each do |key, value|
-    if value <= lowest_value
+    if lowest_name == nil
       lowest_value = value
+      lowest_name = key
+      
+    elsif value < lowest_value
+         lowest_value = value
       lowest_name = key
     end
   end
