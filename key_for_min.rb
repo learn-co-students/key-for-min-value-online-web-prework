60f1969f99e466de3b878cf37ca0f05e2e89651e
Jasmine Chen <jasmine1226@gmail.com>
2019-05-13 01:16:33 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,12 +13,9 @@ def key_for_min_value(name_hash)
         min_key = key
         min_value = value
         first = true
-      else
-        if value < min_value
+      elsif value < min_value
           min_key = key
           min_value = value
-
-        end
       end
     end
   end
